4eb02c50cf1aa3a256aa8916bea1e734709240f8
Sascha <146263983+gigipink1@users.noreply.github.com>
2023-10-14 18:52:42 +0100

    Add functionality to calculator buttons

--- script.js
+++ script.js
@@ -1,91 +1,86 @@
+function add (a,b) {
+    return a+b;
+}
+
+function subtract (a,b) {
+    return a-b;
+}
+
+function multiply (a,b) {
+    return a*b;
+}
+
+function divide (a,b) {
+    if (b === 0) {
+        return "Come on, man";
+    } else {
+    return (a/b).toFixed(3);
+    }
+}
+
+let numOne = '';
+let numTwo = '';
+let operator = '';
+
+function operate (a, op, b) {
+    if (op === "+") {
+       return add(a,b);
+    } else if (op === "-" ) {
+       return subtract(a,b);
+    } else if (op === "*") {
+       return multiply(a,b);
+    } else if (op === "/") {
+       return divide(a,b);
+    }
+    
+}
+
 const numbers = document.querySelector('.numbers');
 const numBtns = numbers.querySelectorAll('button');
 const screen = document.querySelector('.screen');
 
 numBtns.forEach((button) => {
-    button.addEventListener ('click', function() {
-        let number = button.textContent;
-        useNumber(number)})
+    button.addEventListener ('click', () => {
+        const number = button.textContent;
+        if (operator === '=') {
+            screen.textContent = '';
+            operator = '';
+            numTwo = '';
+            numTwo += number;
+            screen.textContent = numTwo;
+        } else if (operator !== '') {
+            numTwo += number;
+            screen.textContent = numTwo;
+        } else  {
+            numOne += number;
+            screen.textContent = numOne;
+        }
+        });
 }); 
-let result = 0;
-
-function useNumber(number) {
-   if (screen.textContent === '+'
-   ||  screen.textContent === '-'
-   ||  screen.textContent === '/'
-   ||  screen.textContent === '*'
-   || screen.textContent == result) {
-    screen.textContent = '';
-   }
-    screen.textContent += number;
-}
 
 const operators = document.querySelector('.operators');
 const opBtns = operators.querySelectorAll('button');
-let operatorHold = '';
-let operatorTwo = '';
-let numOne = '';
-let numTwo = '';
-
-
 
 opBtns.forEach((button) => {
     button.addEventListener('click', () => {
-    const operator = button.textContent 
-    useOperator(operator);
+    const op = button.textContent 
+    if (numTwo !== '') {
+        screen.textContent = operate(parseFloat(numOne), operator, parseFloat(numTwo));
+        operator = op;
+        numOne = screen.textContent;
+        numTwo = '';
+
+    } else if ((numOne === '' && numTwo === '') 
+                || numTwo === '' && op === '=') {
+        screen.textContent = 'ERROR' 
+    } 
+        
+     else {
+
+        screen.textContent = op;
+        operator = op;
+    }
+    
+        
     })
 })
-
-
-function useOperator(operator) {
-    
-    if (numOne === ''){
-         numOne = screen.textContent;
-         screen.textContent = operator;
-         operatorHold = operator;
-         console.log(numOne)
-
-        } else {
-         numTwo = screen.textContent;
-         console.log(numTwo)
-         operatorTwo = operator;
-         doMaths();
-    }
-    
-
-}
-    
-
-function doMaths() {
-    if (operatorHold === '+') {
-        result =  parseInt(numOne) + parseInt(numTwo);
-        screen.textContent = result;
-        numOne = result;
-        numTwo = '';
-        operatorHold = operatorTwo;
-        operatorTwo = ''
-    } else if (operatorHold === '-') {
-        result = parseInt(numOne) - parseInt(numTwo)
-        screen.textContent = result;
-        numOne = result;
-        numTwo = '';
-        operatorHold = operatorTwo;
-        operatorTwo = '';
-    } else if (operatorHold === '*') {
-        result = parseInt(numOne) * parseInt(numTwo)
-        screen.textContent = result;
-        numOne = result;
-        numTwo = '';
-        operatorHold = operatorTwo;
-        operatorTwo = '';
-    } else if (operatorHold === '/') {
-        result = parseInt(numOne) / parseInt(numTwo)
-        screen.textContent = result;
-        numOne = result;
-        numTwo = '';
-        operatorHold = operatorTwo;
-        operatorTwo = '';
-    } else {
-        console.log(operatorHold);
-    }
-}
